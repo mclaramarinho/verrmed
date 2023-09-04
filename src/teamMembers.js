1c22d5f4b13f52371d6711e864cfa5ee33c96fd7
Clara Marinho <marinho.claramb@gmail.com>
2023-09-04 14:21:25 -0300

    avatars

--- src/teamMembers.js
+++ src/teamMembers.js
@@ -3,31 +3,31 @@ const teamMembers = [
         id:1,
         nome:"Clara Marinho",
         funcao:"Desenvolvedora front-end",
-        avatar:"https://i.ibb.co/ZgkR9Pt/clara-marinho.png"
+        avatar:"https://i.ibb.co/ZgkR9Pt/clara-marinho.jpeg"
     },
     {
         id:2,
         nome:"Tatiana Limongi",
         funcao:"Product Owner",
-        avatar:"https://i.ibb.co/n6P5LjT/tatiana-limongi.png"
+        avatar:"https://i.ibb.co/n6P5LjT/tatiana-limongi.jpeg"
     },
     {
         id:3,
         nome:"Yngrid Fernandes",
         funcao: "Planejamento e Análise",
-        avatar: "https://i.ibb.co/wMvgyWg/yngrid-fernandes.png"
+        avatar: "https://i.ibb.co/wMvgyWg/yngrid-fernandes.jpeg"
     },
     {
         id:4,
         nome:"Sérgio Rodrigues",
         funcao: "Planejamento e Análise",
-        avatar: "https://i.ibb.co/VtQnBsD/sergio-rodrigues.png"
+        avatar: "https://i.ibb.co/VtQnBsD/sergio-rodrigues.jpeg"
     },
     {
         id:5,
         nome:"Victor Melo",
         funcao: "WebWriter",
-        avatar: "https://i.ibb.co/K65Rj5x/victor-melo.png"
+        avatar: "https://i.ibb.co/K65Rj5x/victor-melo.jpeg"
     }
 ]
 
